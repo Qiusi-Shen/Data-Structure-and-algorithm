fcd7de42abd296f431a4be3b8a811ecf466708c9
Qiusi-Shen <qiusi@ucla.edu>
2017-02-09 13:26:50 +0800

    Create mazestack.cpp

---
+++ Homework2/mazestack.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Coord
+{
+public:
+	Coord(int rr, int cc) : m_r(rr), m_c(cc) {}
+	int r() const { return m_r; }
+	int c() const { return m_c; }
+private:
+	int m_r;
+	int m_c;
+};
+
+// Return true if there is a path from (sr,sc) to (er,ec)
+// through the maze; return false otherwise
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec)
+{
+	stack<Coord> coordStack;
+	Coord init(sr, sc);
+	coordStack.push(init);
+	maze[sr][sc] = 'X';
+	
+	int row = sr;
+	int col = sc;
+
+	while (!coordStack.empty())
+	{
+		init = coordStack.top();
+		row = init.r();
+		col = init.c();
+		coordStack.pop();
+		if (row == er && col == ec)
+			return true;
+
+		if (row > 0 && maze[row-1][col]!='X')
+		{
+			Coord temp(row - 1, col);
+			coordStack.push(temp);
+			maze[row - 1][col] = 'X';
+		}
+
+		if (col < nCols-1  && maze[row][col+1] != 'X')
+		{
+			Coord temp(row, col+1);
+			coordStack.push(temp);
+			maze[row][col+1] = 'X';
+		}
+
+		if (row < nRows - 1 && maze[row + 1][col] != 'X')
+		{
+			Coord temp(row + 1, col);
+			coordStack.push(temp);
+			maze[row + 1][col] = 'X';
+		}
+
+		if (col > 0 && maze[row][col - 1] != 'X')
+		{
+			Coord temp(row, col - 1);
+			coordStack.push(temp);
+			maze[row][col - 1] = 'X';
+		}
+	}
+	return false;
+}
+
+
+int main()
+{
+	string maze[10] = {
+		"XXXXXXXXXX",
+		"X........X",
+		"XX.X.XXXXX",
+		"X..X.X...X",
+		"X..X...X.X",
+		"XXXX.XXX.X",
+		"X.X....XXX",
+		"X..XX.XX.X",
+		"X...X....X",
+		"XXXXXXXXXX"
+	};
+
+	if (pathExists(maze, 10, 10, 6, 4, 1, 1))
+		cout << "Solvable!" << endl;
+	else
+		cout << "Out of luck!" << endl;
+}
